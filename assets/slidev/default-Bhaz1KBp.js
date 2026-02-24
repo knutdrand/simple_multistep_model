138c65e47a33b692078c2623da235794609fda47
knutdrand <knutdrand@gmail.com>
2026-02-24 10:31:24 +0200

    Deploy slides to GitHub Pages

---
+++ assets/slidev/default-Bhaz1KBp.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-DY4ekD6w.js";import{_ as r}from"../index-Cn79ic0N.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
